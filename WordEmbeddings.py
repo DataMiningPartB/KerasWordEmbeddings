e567bb40622a091c061e5cdb50466522f9a6bbee
florap31 <fpierre1202@eagle.fgcu.edu>
2019-11-25 14:52:10 -0500

    CNN

--- WordEmbeddings.py
+++ WordEmbeddings.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from keras import Sequential
-from keras.layers import Embedding, Dropout, Conv1D, MaxPooling1D, Flatten, Dense, GlobalMaxPooling1D, Activation
+from keras.layers import Embedding, Dropout, Conv1D, MaxPooling1D, Flatten, Dense, GlobalMaxPooling1D, Activation, \
+    GlobalMaxPool1D
 from keras.utils import to_categorical
 from keras_preprocessing.text import Tokenizer
 from nltk.corpus import stopwords
@@ -16,8 +17,8 @@ from keras.preprocessing.sequence import pad_sequences
 
 
 # Loading data
-
 dataset = pd.read_csv("full_amazon_ff_reviews.csv")
+
 # counts null values
 print(dataset.isnull().any().sum())
 # remove null values
@@ -28,23 +29,20 @@ dataset = dataset.dropna(how='any',axis=0)
 X = dataset['Text']
 y = dataset['Rating']
 
-
-
-
 # create training and test sets with test as 28%
-train_data, test_data, train_labels, test_labels = train_test_split(X, y, test_size=0.28, random_state=53)
+train_data, test_data, train_labels, test_labels = train_test_split(X, y, test_size=0.33, random_state=42)
 
 # (train_data, train_labels), (test_data, test_labels) = dataset.load_data(num_words=20000)
 
 print("Training data: {}, labels: {}".format(len(train_data), len(train_labels)))
 # Pre-processing
-num_words = 20000
-max_len = 500
-embedding_dims = 20
+num_words = 10000
+max_len = 10000
+embedding_dims = 50
 filters = 16
 kernel_size = 3
-hidden_dims = 251
-epochs = 10
+hidden_dims = 250
+epochs = 5
 batch_size = 32
 
 tokenizer = Tokenizer(num_words=num_words)
@@ -54,37 +52,54 @@ test_data = tokenizer.texts_to_sequences(test_data)
 
 train_data = pad_sequences(train_data, maxlen=max_len)
 test_data = pad_sequences(test_data, maxlen=max_len)
-print(train_data.shape)
 print(train_data)
 # One-hot encoding needs to be done
 label_encoder = LabelEncoder()
-encoded = label_encoder.fit_transform(y)
-train_labels = to_categorical(encoded)
-test_labels = to_categorical(encoded)
-train_data.transpose()
-test_labels.transpose()
-print(train_labels.shape)
+train_labels = label_encoder.fit_transform(train_labels)
+test_labels = label_encoder.fit_transform(test_labels)
+
 print(train_labels)
+train_labels = to_categorical(train_labels, dtype='float32')
+test_labels = to_categorical(test_labels, dtype='float32')
+
 # creating model
 model = Sequential()
-model.add(Embedding(num_words, embedding_dims,input_length=max_len))
-# prevents overfitting
-model.add(Dropout(0.5))
-model.add(Conv1D(filters, kernel_size,
-                 padding='valid',
-                 activation='relu'))
+# model.add(Embedding(num_words, embedding_dims, input_length=max_len))
+# # prevents overfitting
+# model.add(Dropout(0.2))
+# model.add(Conv1D(filters, kernel_size,
+#                  padding='valid',
+#                  activation='relu', strides=1))
+# # max pooling
+# model.add(GlobalMaxPooling1D())
+# # model.add(Conv1D(filters, kernel_size,
+# #                  padding='valid',
+# #                  activation='relu'))
+# # model.add(MaxPooling1D())
+#
+# # vanilla hidden layer
+# model.add(Dense(hidden_dims, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(1, activation='sigmoid'))
+
+model.add(Embedding(num_words, embedding_dims, input_length=max_len))
+
+
+model.add(Conv1D(hidden_dims,kernel_size ,padding='valid',activation='relu',strides=1))
 model.add(GlobalMaxPooling1D())
+#model.add(MaxPooling1D())
+# prevents overfitting
+
 model.add(Dense(hidden_dims))
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
 model.add(Activation('relu'))
-
-model.add(Dense(1))
+model.add(Dense(3))
 model.add(Activation('sigmoid'))
-
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-
+model.summary()
 model.fit(train_data, train_labels,
           batch_size=batch_size,
           epochs=epochs,
