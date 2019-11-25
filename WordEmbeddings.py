98378ffb5627bec01ff26b6856d154b0b494802c
rwdarrow <rwdarrow4582@eagle.fgcu.edu>
2019-11-24 22:04:52 -0500

    fasttext and CNN working now

--- WordEmbeddings.py
+++ WordEmbeddings.py
@@ -1,9 +1,12 @@
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from keras import Sequential
-from keras.layers import Embedding, Dropout, Conv1D, MaxPooling1D, Flatten, Dense
+from keras.layers import Embedding, Dropout, Conv1D, MaxPooling1D, Flatten, Dense, GlobalMaxPooling1D, Activation
+from keras.utils import to_categorical
 from keras_preprocessing.text import Tokenizer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
@@ -11,7 +14,6 @@ from sklearn.model_selection import train_test_split
 from tensorflow import keras
 
 from keras.preprocessing.sequence import pad_sequences
-
 
 # Loading data
 
@@ -26,21 +28,21 @@ dataset = dataset.dropna(how='any',axis=0)
 X = dataset['Text']
 y = dataset['Rating']
 
+
 # create training and test sets with test as 28%
 train_data, test_data, train_labels, test_labels = train_test_split(X, y, test_size=0.28, random_state=53)
 
 # (train_data, train_labels), (test_data, test_labels) = dataset.load_data(num_words=20000)
 
 print("Training data: {}, labels: {}".format(len(train_data), len(train_labels)))
-
 # Pre-processing
-num_words = 20000
-max_len = 500
-embedding_dims = 20
-filters = 16
+num_words = 5000
+max_len = 400
+embedding_dims = 50
+filters = 250
 kernel_size = 3
 hidden_dims = 250
-epochs = 10
+epochs = 5
 batch_size = 32
 
 tokenizer = Tokenizer(num_words=num_words)
@@ -48,16 +50,37 @@ tokenizer.fit_on_texts(train_data)
 train_data = tokenizer.texts_to_sequences(train_data)
 test_data = tokenizer.texts_to_sequences(test_data)
 
-
 train_data = pad_sequences(train_data, maxlen=max_len)
 test_data = pad_sequences(test_data, maxlen=max_len)
+print(train_data)
+
+# integer encoding
+label_encoder = LabelEncoder()
+train_labels = label_encoder.fit_transform(train_labels)
+test_labels = label_encoder.fit_transform(test_labels)
+print(train_labels)
+
+# one hot encoding
+onehot_encoder = OneHotEncoder(sparse=False)
+train_labels = train_labels.reshape(len(train_labels), 1)
+test_labels = test_labels.reshape(len(test_labels), 1)
+onehot_encoded_train = onehot_encoder.fit_transform(train_labels)
+onehot_encoded_test = onehot_encoder.fit_transform(test_labels)
+print(train_labels)
+
+#train_labels = to_categorical(encoded)
+#test_labels = to_categorical(encoded))
 
 # creating model
-
 model = Sequential()
-model.add(Embedding(num_words, embedding_dims,input_length=max_len))
+model.add(Embedding(num_words, embedding_dims, input_length=max_len))
+
 # prevents overfitting
 model.add(Dropout(0.5))
+model.add(Conv1D(filters, kernel_size,
+                 padding='valid',
+                 activation='relu'))
+model.add(MaxPooling1D())
 model.add(Conv1D(filters, kernel_size,
                  padding='valid',
                  activation='relu'))
@@ -65,7 +88,7 @@ model.add(MaxPooling1D())
 model.add(Flatten())
 model.add(Dense(hidden_dims, activation='relu'))
 model.add(Dropout(0.5))
-
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
@@ -75,3 +98,8 @@ model.fit(train_data, train_labels,
           batch_size=batch_size,
           epochs=epochs,
           validation_data=(test_data, test_labels))
+
+loss, accuracy = model.evaluate(train_data, train_labels, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(test_data, test_labels, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
---
+++ fasttext.py
@@ -0,0 +1,156 @@
+import numpy as np
+
+import pandas as pd
+from keras.preprocessing import sequence
+from keras_preprocessing.text import Tokenizer
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers import GlobalAveragePooling1D
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+
+def create_ngram_set(input_list, ngram_value=2):
+    """
+    Extract a set of n-grams from a list of integers.
+
+    >>> create_ngram_set([1, 4, 9, 4, 1, 4], ngram_value=2)
+    {(4, 9), (4, 1), (1, 4), (9, 4)}
+
+    >>> create_ngram_set([1, 4, 9, 4, 1, 4], ngram_value=3)
+    [(1, 4, 9), (4, 9, 4), (9, 4, 1), (4, 1, 4)]
+    """
+    return set(zip(*[input_list[i:] for i in range(ngram_value)]))
+
+
+def add_ngram(sequences, token_indice, ngram_range=2):
+    """
+    Augment the input list of list (sequences) by appending n-grams values.
+
+    Example: adding bi-gram
+    >>> sequences = [[1, 3, 4, 5], [1, 3, 7, 9, 2]]
+    >>> token_indice = {(1, 3): 1337, (9, 2): 42, (4, 5): 2017}
+    >>> add_ngram(sequences, token_indice, ngram_range=2)
+    [[1, 3, 4, 5, 1337, 2017], [1, 3, 7, 9, 2, 1337, 42]]
+
+    Example: adding tri-gram
+    >>> sequences = [[1, 3, 4, 5], [1, 3, 7, 9, 2]]
+    >>> token_indice = {(1, 3): 1337, (9, 2): 42, (4, 5): 2017, (7, 9, 2): 2018}
+    >>> add_ngram(sequences, token_indice, ngram_range=3)
+    [[1, 3, 4, 5, 1337, 2017], [1, 3, 7, 9, 2, 1337, 42, 2018]]
+    """
+    new_sequences = []
+    for input_list in sequences:
+        new_list = input_list[:]
+        for ngram_value in range(2, ngram_range + 1):
+            for i in range(len(new_list) - ngram_value + 1):
+                ngram = tuple(new_list[i:i + ngram_value])
+                if ngram in token_indice:
+                    new_list.append(token_indice[ngram])
+        new_sequences.append(new_list)
+
+    return new_sequences
+
+# Set parameters:
+# ngram_range = 2 will add bi-grams features
+ngram_range = 1
+max_features = 20000
+maxlen = 400
+num_words = 5000
+batch_size = 32
+embedding_dims = 50
+epochs = 5
+
+print('Loading data...')
+# Loading data
+
+dataset = pd.read_csv("full_amazon_ff_reviews.csv")
+# counts null values
+print(dataset.isnull().any().sum())
+# remove null values
+dataset = dataset.dropna(how='any',axis=0)
+
+
+# x & y holds values
+X = dataset['Text']
+y = dataset['Rating']
+
+train_data, test_data, train_labels, test_labels = train_test_split(X, y, test_size=0.28, random_state=53)
+print(len(train_data), 'train sequences')
+print(len(test_data), 'test sequences')
+print('Average train sequence length: {}'.format(
+    np.mean(list(map(len, train_data)), dtype=int)))
+print('Average test sequence length: {}'.format(
+    np.mean(list(map(len, test_data)), dtype=int)))
+
+if ngram_range > 1:
+    print('Adding {}-gram features'.format(ngram_range))
+    # Create set of unique n-gram from the training set.
+    ngram_set = set()
+    for input_list in train_data:
+        for i in range(2, ngram_range + 1):
+            set_of_ngram = create_ngram_set(input_list, ngram_value=i)
+            ngram_set.update(set_of_ngram)
+
+    # Dictionary mapping n-gram token to a unique integer.
+    # Integer values are greater than max_features in order
+    # to avoid collision with existing features.
+    start_index = max_features + 1
+    token_indice = {v: k + start_index for k, v in enumerate(ngram_set)}
+    indice_token = {token_indice[k]: k for k in token_indice}
+
+    # max_features is the highest integer that could be found in the dataset.
+    max_features = np.max(list(indice_token.keys())) + 1
+
+    # Augmenting x_train and x_test with n-grams features
+    x_train = add_ngram(train_data, token_indice, ngram_range)
+    x_test = add_ngram(test_data, token_indice, ngram_range)
+    print('Average train sequence length: {}'.format(
+        np.mean(list(map(len, x_train)), dtype=int)))
+    print('Average test sequence length: {}'.format(
+        np.mean(list(map(len, x_test)), dtype=int)))
+
+tokenizer = Tokenizer(num_words=num_words)
+tokenizer.fit_on_texts(train_data)
+train_data = tokenizer.texts_to_sequences(train_data)
+test_data = tokenizer.texts_to_sequences(test_data)
+
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(train_data, maxlen=maxlen)
+x_test = sequence.pad_sequences(test_data, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+# integer encoding
+label_encoder = LabelEncoder()
+train_labels = label_encoder.fit_transform(train_labels)
+test_labels = label_encoder.fit_transform(test_labels)
+print(train_labels)
+
+train_labels = to_categorical(train_labels, dtype='float32')
+test_labels = to_categorical(test_labels, dtype='float32')
+
+
+print('Build model...')
+model = Sequential()
+
+# we start off with an efficient embedding layer which maps
+# our vocab indices into embedding_dims dimensions
+model.add(Embedding(max_features,
+                    embedding_dims,
+                    input_length=maxlen))
+
+# we add a GlobalAveragePooling1D, which will average the embeddings
+# of all words in the document
+model.add(GlobalAveragePooling1D())
+
+# We project onto a single unit output layer, and squash it with a sigmoid:
+model.add(Dense(3, activation='sigmoid'))
+
+model.compile(loss="categorical_crossentropy",optimizer='rmsprop',metrics=['acc'])
+
+model.fit(x_train, train_labels,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, test_labels))
